312ef4e10a3fb54d3aa65a4a41beb54c0ef33098
ka01 <kaoru.kmt.21@gmail.com>
2019-08-04 18:49:23 +0900

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  # Basic認証
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'frema51a' && password == '9074'
+    end
+  end
 end
