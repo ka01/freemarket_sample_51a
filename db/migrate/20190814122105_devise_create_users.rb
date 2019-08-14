4852efb83b595b52c17de886ac9350cd69f770a2
yasuharus <harukuroa@icloud.com>
2019-08-14 21:25:36 +0900

    usersモデルを作成

--- db/migrate/20190814122105_devise_create_users.rb
+++ db/migrate/20190814122105_devise_create_users.rb
@@ -4,8 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
+
+      t.string  :family_name,        null: false
+      t.string  :first_name,         null: false
+      t.string  :family_name_kana,   null: false
+      t.string  :first_name_kana ,   null: false
+      t.integer :birth_year,         null: false
+      t.integer :birth_month,        null: false
+      t.integer :birth_day,          null: false
+      t.integer  :status,            null: false, default: "0"
+      t.datetime :deleted_at
+
 
       ## Recoverable
       t.string   :reset_password_token
