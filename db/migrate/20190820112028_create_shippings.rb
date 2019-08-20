2fea551d308e0ae8df526743ad5dceb857e104e5
div-antk <51283468+owaii@users.noreply.github.com>
2019-08-20 20:29:26 +0900

    shippingテーブルを追加

---
+++ db/migrate/20190820112028_create_shippings.rb
@@ -0,0 +1,11 @@
+class CreateShippings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shippings do |t|
+      t.boolean :fee_burden, null: false
+      t.integer :service, null: false
+      t.integer :area, null: false
+      t.integer :handling_time, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_222247) do
+ActiveRecord::Schema.define(version: 2019_08_20_112028) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "prefecture_id"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "brand_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,12 +48,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_222247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_item_images_on_user_id"
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,6 +57,15 @@ ActiveRecord::Schema.define(version: 2019_08_19_222247) do
     t.string "text", null: false
     t.integer "price", null: false
     t.string "condition", null: false
+  end
+
+  create_table "shippings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.boolean "fee_burden", null: false
+    t.integer "service", null: false
+    t.integer "area", null: false
+    t.integer "handling_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
