dd3a1a488b08e13462242373646d66cb311e5a4f
eggchi21 <eggchi21@yahoo.co.jp>
2019-08-31 12:14:09 +0900

    edit pirapira

--- app/assets/javascripts/muitinav.js
+++ app/assets/javascripts/muitinav.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
   $('.nav-category').hover(function(){
-    $(this).addClass('active');
-    $('.active').children('.category-1').show();
+    $(this).addClass('hover-category-title');
+    $('.hover-category-title').children('.category-1').show();
   },function(){
-    $(this).removeClass('active');
+    $(this).removeClass('hover-category-title');
     $(this).children('.category-1').hide();
   });
 
   $('.category-1__item').hover(function(){
-    $(this).addClass('active2');
-    var grandChild = $('.active2').children('.grand-child-wrap');
-    grandChild.show();
+    $(this).addClass('hover-category-1');
+    $('.hover-category-1').children('.category-2').show();
   },function(){
-    $(this).removeClass('active2');
-    $(this).children('.grand-child-wrap').hide();
+    $(this).removeClass('hover-category-1');
+    $(this).children('.category-2').hide();
+  });
 
+  $('.category-2__item').hover(function(){
+    $(this).addClass('hover-category-2');
+    $('.hover-category-2').children('.category-3').show();
+  },function(){
+    $(this).removeClass('hover-category-2');
+    $(this).children('.category-3').hide();
   });
 });
