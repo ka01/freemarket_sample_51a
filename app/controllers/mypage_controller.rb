d34c84f6155359d07c482f193cb2eab96037a178
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-01 20:31:05 +0900

    prefixを使用

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,11 +1,9 @@
 class MypageController < ApplicationController
   before_action :authenticate_user!
   def index
-    @user=User.find(current_user.id)
   end
 
   def profile
-    @user=User.find(current_user.id)
   end
 
   def notification
