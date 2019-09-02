a28b83b00cf2379157a90750c6915e78a93d8c40
div-antk <51283468+owaii@users.noreply.github.com>
2019-09-02 14:06:52 +0900

    categoryテーブルの実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach('db/category.csv', headers: true) do |row|
+  Category.create(
+    path: row[0],
+    name: row[1]
+  )
+end
