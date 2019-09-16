2322130b4d732dbb79600c5f51f551ebe22e8d42
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-16 22:30:42 +0900

    edit controller for ancestry

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -4,5 +4,9 @@ class CategoryController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @children=@category.children.sort_by{rand}
+    @items=[]
+    @category.subtree.map{|target| @items.push(target.items).flatten!}
   end
 end
