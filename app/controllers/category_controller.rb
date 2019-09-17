ced751a3846b50320e145b76d4735b4aa6401aa6
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-18 00:42:04 +0900

    add gem 'kaminari' & edit pagenatuon

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -8,5 +8,6 @@ class CategoryController < ApplicationController
     @children=@category.children.sort_by{rand}
     @items=[]
     @category.subtree.map{|target| @items.push(target.items).flatten!}
+    @items=Kaminari.paginate_array(@items).page(params[:page]).per(12)
   end
 end
