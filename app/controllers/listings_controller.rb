1add9f3509c7e76aaf3302824d2ad46dce127dd0
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-20 20:47:28 +0900

    add pagenattion to mypaeg-listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,13 @@
 class ListingsController < ApplicationController
   def listing
-    @items=Item.where(seller_id:current_user.id,trading_status:0)
+    @items=Item.where(seller_id:current_user.id,trading_status:0).page(params[:page]).per(12)
   end
 
   def in_progress
-    @items=Item.where(seller_id:current_user.id,trading_status:1)
+    @items=Item.where(seller_id:current_user.id,trading_status:1..2).page(params[:page]).per(12)
   end
 
   def completed
-    @items=Item.where(seller_id:current_user.id,trading_status:2)
+    @items=Item.where(seller_id:current_user.id,trading_status:3).page(params[:page]).per(12)
   end
 end
