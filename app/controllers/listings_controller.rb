63283a20c26f688cf8a9ed48a37c423bcb70df18
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-20 20:20:22 +0900

    add trading_status to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,13 @@
 class ListingsController < ApplicationController
   def listing
-    @items=Item.where(seller_id:current_user.id)
+    @items=Item.where(seller_id:current_user.id,trading_status:0)
   end
 
   def in_progress
-    @items=Item.where(seller_id:current_user.id)
+    @items=Item.where(seller_id:current_user.id,trading_status:1)
   end
 
   def completed
-    @items=Item.where(seller_id:current_user.id)
+    @items=Item.where(seller_id:current_user.id,trading_status:2)
   end
 end
