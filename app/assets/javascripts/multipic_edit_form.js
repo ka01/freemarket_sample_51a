f2fcdc1bd7d8317f0e08194607b6146d0c3c6ffc
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-29 19:49:48 +0900

    add bloburl

--- app/assets/javascripts/multipic_edit_form.js
+++ app/assets/javascripts/multipic_edit_form.js
@@ -13,6 +13,19 @@ $(function(){
           'width': `calc(620px - (125px * ${(image_count+1) % 5 }))`
         })
   }
+  function appendPreview(img){
+    var previewHtml ='';
+    previewHtml =`<li class="re__sell-upload-item">
+                  <img src="${img}" width="114" height="116">
+                  <div class="re__btn_wrapper">
+                    <label class="btn re__edit" for="re__sell-upload-drop-box__file">編集
+                      <input id="re__sell-upload-drop-box__file" class="re__sell-upload-drop-box__file" type="file" data-image="3" name="item[item_images_attributes][3][image_url]">
+                    <input value="843" type="hidden" name="item[item_images_attributes][3][id]" id="item_item_images_attributes_3_id">
+                    </label><div class="btn re__delete">削除</div>
+                  </div>
+                  </li>`
+    $('.re__sell-upload-items').append(previewHtml);
+  }
   function renumbering(){
     var i = 0;
     $('.re__sell-upload-drop-box__file').each(function(i,elem){
@@ -27,6 +40,15 @@ $(function(){
         appendDropBox(image_count)
     }
   });
+  $(document).on('change', 'input[type= "file"].add__sell-upload-drop-box__file',function(e) {
+    console.log('ok')
+    var file = $(this).prop('files')[0];
+    var blobUrl = window.URL.createObjectURL(file);
+    appendPreview(blobUrl)
+    image_count=$('.re__sell-upload-drop-box__file').length - 1
+    appendDropBox(image_count)
+  })
+
   $(document).on('click', '.re__delete', function() {
     var target_image = $(this).parent().parent();
     target_image.remove();
