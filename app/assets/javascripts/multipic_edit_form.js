b21eeb17925c66d42daad5a04ece2c090ce19c21
eggchi21 <eggchi21@yahoo.co.jp>
2019-09-29 17:56:35 +0900

    modify delete method

--- app/assets/javascripts/multipic_edit_form.js
+++ app/assets/javascripts/multipic_edit_form.js
@@ -2,7 +2,7 @@ $(function(){
   function appendDropBox(image_count){
     var dropboxHtml = '';
     dropboxHtml = `<li class="add__sell-upload-drop-box have-item-${(image_count+1) % 5}">
-                        <input class="add__sell-upload-drop-box__file" data-image="${image_count + 1}" id="add__sell-upload-drop-box__file" name="item[item_images_attributes][${image_count+1}][image_url]" type="file">
+                        <input class="add__sell-upload-drop-box__file" id="add__sell-upload-drop-box__file" name="item[item_images_attributes][${image_count+1}][image_url]" type="file">
                         <label class="add__sell-upload-drop-box__text" for="add__sell-upload-drop-box__file">
                           <p >ドラッグアンドドラッグドロップ<br>またはクリックしてファイルをアップロード</p>
                         </label>
@@ -10,23 +10,34 @@ $(function(){
     $('.add__sell-upload-drop-box').remove()
     $('.re__sell-upload-items').append(dropboxHtml);
     $('.add__sell-upload-drop-box').css({
-          'width': `calc(100% - (137px * ${(image_count+1) % 5 }))`
+          'width': `calc(620px - (130px * ${(image_count+1) % 5 }))`
         })
+  }
+  function renumbering(){
+    var i = 0;
+    $('.re__sell-upload-drop-box__file').each(function(i,elem){
+      $(elem).attr('data-image',i);
+      i++;
+    })
   }
 
   $(window).bind("load", function(){
     if(document.URL.match(/\/items\/\d+\/edit/)) {
-        console.log('ok');
         var image_count = $('.re__sell-upload-drop-box__file').last().data('image')
-        console.log(image_count);
         appendDropBox(image_count)
     }
   });
   $(document).on('click', '.re__delete', function() {
     var target_image = $(this).parent().parent();
-    var image_count = $('.re__sell-upload-drop-box__file').last().data('image')
     target_image.remove();
-    appendDropBox(image_count-1)
+    $.when(
+    renumbering()
+    ).done(function(){
+    image_count=$('.re__sell-upload-drop-box__file').length - 1
+    appendDropBox(image_count)
+    })
+
+
   })
 
   })
