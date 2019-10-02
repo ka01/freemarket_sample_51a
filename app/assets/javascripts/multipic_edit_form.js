f791c0ec766e4dbaadce9186acaa25b2e6880bfe
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-02 19:41:31 +0900

    add comment

--- app/assets/javascripts/multipic_edit_form.js
+++ app/assets/javascripts/multipic_edit_form.js
@@ -1,8 +1,11 @@
 $(function(){
-  function appendDropBox(image_count){
+  picture_num = ""
+  delete_item =[]
+
+  function appendDropBox(image_count,picture_num){
     var dropboxHtml = '';
     dropboxHtml = `<li class="re__sell-upload-drop-box drophere">
-                    <input id="re__sell-upload-drop-box__file_${image_count}" class="re__sell-upload-drop-box__file" data-image="${image_count}" name="item[item_images_attributes][${image_count}][image_url]" type="file">
+                    <input id="re__sell-upload-drop-box__file_${image_count}" class="re__sell-upload-drop-box__file" data-image="${image_count}" name="item[item_images_attributes][${picture_num}]][image_url]" type="file">
                     <label class="re__sell-upload-drop-box__text" for="re__sell-upload-drop-box__file_${image_count}">
                       <p >ドラッグアンドドラッグドロップ<br>またはクリックしてファイルをアップロード</p>
                     </label>
@@ -13,34 +16,11 @@ $(function(){
           'width': `calc(620px - (125px * ${(image_count) % 5 }))`
         })
   }
-  function appendPreview(img,image_count,file){
-    var previewHtml ='';
-    previewHtml =`<li class="re__sell-upload-item">
-                  <img src="/uploads/item_image/image_url/859/sample.jpg" width="114" height="116">
-                  <div class="re__btn_wrapper">
-                  <label class="btn re__edit" for="re__sell-upload-drop-box__file_4">編集
-                  <input id="re__sell-upload-drop-box__file_4" class="re__sell-upload-drop-box__file" type="file" data-image="4" name="item[item_images_attributes][4][image_url]">
-                  <input value="859" type="hidden" name="item[item_images_attributes][4][id]" id="item_item_images_attributes_4_id">
-                  </label>
-                  <div class="btn re__delete">削除</div>
-                  </div>
-                  <li class="re__sell-upload-item">
-                  <img src="blob:http://localhost:3000/c6b338ea-8335-408e-b3bc-cc2208d5cfb1" width="114" height="116">
-                    <div class="re__btn_wrapper">
-                    <label class="btn re__edit" for="re__sell-upload-drop-box__file_5">編集
-                    <input id="re__sell-upload-drop-box__file_5" class="re__sell-upload-drop-box__file" data-image="5" name="item[item_images_attributes][5][image_url]" type="file">
-                    </label>
-                    <div class="btn re__delete">削除</div>
-                    </div>
-                  </li>`
-
-    $('.re__sell-upload-items').append(previewHtml);
-  }
   function renumbering(){
     var i = 0;
     $('.re__sell-upload-drop-box__file').each(function(i,elem){
       $(elem).attr('id',`re__sell-upload-drop-box__file_${i}`);
-      $(elem).attr('name',`item[item_images_attributes][${i}][image_url]`);
+      $(elem).parent().attr('for',`re__sell-upload-drop-box__file_${i}`);
       $(elem).attr('data-image',i);
       i++;
     })
@@ -48,41 +28,38 @@ $(function(){
 
   $(window).bind("load", function(){
     if(document.URL.match(/\/items\/\d+\/edit/)) {
-        var image_count = $('.re__sell-upload-drop-box__file').length //既存5枚の場合,drophereはこの時点でないので"5"
-        console.log(image_count)
-        appendDropBox(image_count)
+        var image_count = $('.re__sell-upload-drop-box__file').last().data('image')  //既存5枚の場合,drophereはこの時点でないので"5" ※①
+        picture_num = image_count                                                    //Editビュー初期表示ではpicture_numとimage_countは揃える"5" ※②
+        appendDropBox(image_count+1,picture_num+1)                                   //※②①を引数にDrophereを作成
     }
   });
   $(document).on('change', 'input[type= "file"]',function(e) {
     if ($(this).parent().hasClass('drophere')){
-      var image_count = $('.re__sell-upload-drop-box__file').length  //既存5枚の場合,drophereも含めて"6"
-      var file = $(this).prop('files')[0];
+      var image_count = $('.re__sell-upload-drop-box__file').last().data('image')  //既存5枚の場合,drophereも含めて"6"
+      picture_num += 1                                                             //inputに画像が１枚入ったのでpicture_numに1+する 既存5枚の場合"6" ※③
+      var file = $(this).prop('files')[0];                                         //以下inputタグに写真データを入れたままゴリ押しでプレビュー表示まで成形(inputデータ自体を取得するのはできないらしい)
       var blobUrl = window.URL.createObjectURL(file);
       var preview =`<img src="${blobUrl}" width="114" height="116">`
       $('.re__sell-upload-drop-box__text').remove();
       $(this).wrap(`<div class="re__btn_wrapper"><label class="btn re__edit" for="re__sell-upload-drop-box__file_${image_count-1}">編集</label><div class="btn re__delete">削除</div></div>`)
       $('.re__sell-upload-drop-box').prepend(preview).removeAttr('style').removeClass("re__sell-upload-drop-box drophere").addClass("re__sell-upload-item")
-
       $.when(
-      renumbering()
+      renumbering()                                                               //idとimage-data,親のlabelのforの連番を再配布
       ).done(function(){
-      image_count=$('.re__sell-upload-drop-box__file').length - 1
+        image_count = $('.re__sell-upload-drop-box__file').length                 //既存5枚の場合,成形した1枚も含めて"6" ※④
+        appendDropBox(image_count,picture_num + 1)                                //※④③を引数にDrophereを作成
       })
-      image_count = $('.re__sell-upload-drop-box__file').length
-      appendDropBox(image_count)
     }
   })
-
-  $(document).on('click', '.re__delete', function() {
-    $(this).parent().parent().remove();
-    $.when(
-    renumbering()
-    ).done(function(){
-    image_count = $('.re__sell-upload-drop-box__file').length //既存5枚で1枚削除した場合,drophereも含めて"5"
-    appendDropBox(image_count - 1)
+    $(document).on('click', '.re__delete', function() {
+      $(this).parent().parent().remove();                                         //削除ボタンの親の親ごと削除
+      var test = $(this).prev().children().attr('value')                          //削除ボタンの上隣の子どものvalue取得
+      console.log(test)
+      $.when(
+      renumbering()                                                               //idとimage-data,親のlabelのforの連番を再配布
+      ).done(function(){
+      image_count = $('.re__sell-upload-drop-box__file').length                   //既存5枚で1枚削除した場合,drophereも含めて"5"
+      appendDropBox(image_count - 1, picture_num + 1)
+      })
     })
-
-
-  })
-
   })
