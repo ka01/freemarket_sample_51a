cbe031145ed0354edbd785ce82b516305d9ca106
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-03 21:52:33 +0900

    edit layout

--- app/assets/javascripts/multipic_edit_form.js
+++ app/assets/javascripts/multipic_edit_form.js
@@ -12,8 +12,13 @@ $(function(){
     $('.drophere').remove()
     $('.re__sell-upload-items').append(dropboxHtml);
     $('.drophere').css({
-          'width': `calc(620px - (125px * ${(image_count) % 5 }))`
-        })
+          'width': `calc(620px - (134px * ${(image_count) % 5 }))`
+    })
+    if (image_count == 4 ||image_count == 9 ){
+      $('.re__sell-upload-drop-box__text').css({
+        'padding': '10px'
+      })
+    }
   }
   function renumbering(){
     var i = 0;
@@ -43,10 +48,12 @@ $(function(){
       $(this).wrap(`<div class="re__btn_wrapper"><label class="btn re__edit" for="re__sell-upload-drop-box__file_${image_count-1}">編集</label><div class="btn re__delete">削除</div></div>`)
       $('.re__sell-upload-drop-box').prepend(preview).removeAttr('style').removeClass("re__sell-upload-drop-box drophere").addClass("re__sell-upload-item")
       $.when(
-      renumbering()                                                               //idとimage-data,親のlabelのforの連番を再配布
+        renumbering()                                                               //idとimage-data,親のlabelのforの連番を再配布
       ).done(function(){
         image_count = $('.re__sell-upload-drop-box__file').length                 //既存5枚の場合,成形した1枚も含めて"6" ※④
+        if (image_count != 10){
         appendDropBox(image_count, picture_num + 1)                                //※④,③+1を引数にDrophereを作成
+        }
       })
     }
   })
@@ -55,10 +62,19 @@ $(function(){
     $(this).prev().remove()                                                       //削除ボタンの直上の編集ボタンを削除 '.re__sell-upload-drop-box__file'の数を削除の分だけ減らすため
     $(this).find('.re__sell-upload-drop-box__delete-flag').prop('checked', true)  //削除対象のcheck_boxにチェックを入れる
     $.when(
-    renumbering()                                                                 //idとimage-data,親のlabelのforの連番を再配布
+      renumbering()                                                                 //idとimage-data,親のlabelのforの連番を再配布
     ).done(function(){
-    image_count = $('.re__sell-upload-drop-box__file').length                     //既存5枚で1枚削除した場合,drophereも含めて"5"
-    appendDropBox(image_count - 1, picture_num + 1)                               //削除後の写真の数-1 と 一度も追加したことない場合②+1,一度でも追加したら③+1を引数にDrophereを作成
+      image_count = $('.re__sell-upload-drop-box__file').length                     //既存5枚で1枚削除した場合,drophereも含めて"5"
+      console.log(image_count )
+      if (image_count == 9){
+        if ($('.re__sell-upload-drop-box__file').last().parent().hasClass("drophere")){
+          appendDropBox(image_count-1 , picture_num + 1)
+        }else{
+          appendDropBox(image_count , picture_num + 1)
+        }
+      }else{
+        appendDropBox(image_count - 1, picture_num + 1)                               //削除後の写真の数-1 と 一度も追加したことない場合②+1,一度でも追加したら③+1を引数にDrophereを作成
+      }
     })
   })
 })
