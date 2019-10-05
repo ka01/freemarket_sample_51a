375383859fc788f10242fe4531286415e7b80e43
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-06 07:46:09 +0900

    edit intend

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   def create
-    @like=Like.where(user_id: current_user.id, item_id: params[:id])
-    Like.create(user_id: current_user.id,item_id: params[:id])     if @like.length==0
-    @likes=Like.where(item_id: params[:id])
+    @like = Like.where(user_id: current_user.id, item_id: params[:id])
+    Like.create(user_id: current_user.id,item_id: params[:id])     if @like.length == 0
+    @likes = Like.where(item_id: params[:id])
     respond_to do |format|
       format.html { redirect_to :root }
       format.json { render json: @likes}
@@ -11,9 +11,9 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like=Like.find_by(user_id: current_user.id,item_id: params[:id])
+    @like = Like.find_by(user_id: current_user.id,item_id: params[:id])
     @like.destroy
-    @likes=Like.where(item_id: params[:id])
+    @likes = Like.where(item_id: params[:id])
     respond_to do |format|
       format.html { redirect_to :root }
       format.json { render json: @likes}
